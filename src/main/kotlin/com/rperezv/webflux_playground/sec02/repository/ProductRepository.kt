65f2ac49f51c42e5cb259987071c8e38f82b243f
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-05-14 21:44:39 +0200

    26. Pageable

--- src/main/kotlin/com/rperezv/webflux_playground/sec02/repository/ProductRepository.kt
+++ src/main/kotlin/com/rperezv/webflux_playground/sec02/repository/ProductRepository.kt
@@ -1,6 +1,7 @@
 package com.rperezv.webflux_playground.sec02.repository
 
 import com.rperezv.webflux_playground.sec02.domain.Product
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
@@ -12,5 +13,7 @@ interface ProductRepository : ReactiveMongoRepository<Product, String> {
     fun findByDescription(description: String): Mono<Product>
 
     fun findByPriceBetween(from: Int, to: Int): Flux<Product>
+
+    fun findAllBy(pageable: Pageable): Flux<Product>
 
 }
--- src/test/kotlin/com/rperezv/webflux_playground/sec02/Lec02ProductRepositoryTest.kt
+++ src/test/kotlin/com/rperezv/webflux_playground/sec02/Lec02ProductRepositoryTest.kt
@@ -2,8 +2,11 @@ package com.rperezv.webflux_playground.sec02
 
 import com.rperezv.webflux_playground.sec02.repository.ProductRepository
 import mu.KLogging
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import reactor.test.StepVerifier
 
 class Lec02ProductRepositoryTest : AbstractTest() {
@@ -20,6 +23,21 @@ class Lec02ProductRepositoryTest : AbstractTest() {
             .doOnNext { p -> logger.info("retrievedProduct: $p") }
             .let { StepVerifier.create(it) }
             .expectNextCount(3)
+            .expectComplete()
+            .verify()
+
+    }
+
+    @Test
+    fun findAllBy() {
+        val pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.ASC, "price"))
+
+        productRepository.findAllBy(pageRequest)
+            .doOnNext { p -> logger.info("retrievedProduct: $p") }
+            .let { StepVerifier.create(it) }
+            .assertNext { p -> Assertions.assertEquals(200, p.price) }
+            .assertNext { p -> Assertions.assertEquals(250, p.price) }
+            .assertNext { p -> Assertions.assertEquals(300, p.price) }
             .expectComplete()
             .verify()
 
