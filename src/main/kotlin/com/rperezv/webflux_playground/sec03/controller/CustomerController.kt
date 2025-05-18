6a9d9b27d6869f947dd148c01e8997aaded0e1aa
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-05-18 18:11:44 +0200

    62. Validation - Emiting Error Signal

--- src/main/kotlin/com/rperezv/webflux_playground/sec03/controller/CustomerController.kt
+++ src/main/kotlin/com/rperezv/webflux_playground/sec03/controller/CustomerController.kt
@@ -2,17 +2,11 @@ package com.rperezv.webflux_playground.sec03.controller
 
 import com.rperezv.webflux_playground.sec03.dto.CustomerDto
 import com.rperezv.webflux_playground.sec03.service.CustomerService
+import com.rperezv.webflux_playground.sec04.exceptions.ApplicationExceptions
+import com.rperezv.webflux_playground.sec04.validator.RequestValidator
 import org.springframework.data.domain.Page
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -34,28 +28,34 @@ class CustomerController(
     }
 
     @DeleteMapping(CUSTOMER_PATH_ID)
-    fun deleteCustomer(@PathVariable customerId: String): Mono<ResponseEntity<Void>> {
+    fun deleteCustomer(@PathVariable customerId: String): Mono<Void> {
+
         return customerService.deleteCustomerById(customerId)
             .filter { it }
-            .map { ResponseEntity.ok().build<Void>() }
-            .defaultIfEmpty(ResponseEntity.notFound().build())
+            .switchIfEmpty(ApplicationExceptions.customerNotFound(customerId))
+            .then()
+
     }
 
     @PutMapping(CUSTOMER_PATH_ID)
     fun updateCustomer(@PathVariable customerId: String, @RequestBody monoDto: Mono<CustomerDto>)
-                : Mono<ResponseEntity<CustomerDto>>
-            = customerService.updateCustomer(customerId, monoDto)
-                .map { updateDto -> ResponseEntity.ok(updateDto) }
-                .defaultIfEmpty(ResponseEntity.notFound().build())
+                : Mono<CustomerDto> {
 
+        return monoDto.transform(RequestValidator.validate())
+            .`as`{ validatedMonoDto -> customerService.updateCustomer(customerId, validatedMonoDto) }
+            .switchIfEmpty(ApplicationExceptions.customerNotFound(customerId))
+
+    }
 
     @PostMapping(CUSTOMER_PATH)
     fun saveCustomer(@RequestBody monoDto: Mono<CustomerDto>): Mono<ResponseEntity<Void>> {
 
-        return customerService.saveCustomer(monoDto).map { saveDto ->
-            ResponseEntity.created(UriComponentsBuilder
-                .fromPath("/api/v1$CUSTOMER_PATH_ID")
-                .build(saveDto.id)
+        return monoDto.transform(RequestValidator.validate())
+            .`as`(customerService::saveCustomer)
+            .map { saveDto ->
+                ResponseEntity.created(UriComponentsBuilder
+                    .fromPath("/api/v1$CUSTOMER_PATH_ID")
+                    .build(saveDto.id)
             ).build()
         }
 
@@ -65,11 +65,10 @@ class CustomerController(
     fun allCustomers(): Flux<CustomerDto> = customerService.getAllCustomers()
 
     @GetMapping(CUSTOMER_PATH_ID)
-    fun getCustomer(@PathVariable customerId: String): Mono<ResponseEntity<CustomerDto>> {
+    fun getCustomer(@PathVariable customerId: String): Mono<CustomerDto> {
 
         return customerService.getCustomerById(customerId)
-            .map { dto -> ResponseEntity.ok(dto) }
-            .defaultIfEmpty(ResponseEntity.notFound().build())
+            .switchIfEmpty(ApplicationExceptions.customerNotFound(customerId))
     }
 
 }
