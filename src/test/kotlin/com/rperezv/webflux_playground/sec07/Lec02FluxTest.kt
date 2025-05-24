0ee27aefdba2cb208bbcd573eb624c4fa4e32fda
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-05-24 17:02:17 +0200

    101. Streaming GET

---
+++ src/test/kotlin/com/rperezv/webflux_playground/sec07/Lec02FluxTest.kt
@@ -0,0 +1,29 @@
+package com.rperezv.webflux_playground.sec07
+
+import com.rperezv.webflux_playground.sec07.dto.Product
+import org.junit.jupiter.api.Test
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.test.StepVerifier
+import java.time.Duration
+
+class Lec02FluxTest : AbstractWebClient() {
+
+    private val client: WebClient = createWebClient()
+
+    @Test
+    fun streamingResponse() {
+
+        this.client.get()
+            .uri("/{lec}/product/stream", "lec02")
+            .retrieve()
+            .bodyToFlux(Product::class.java)
+            .take(Duration.ofSeconds(3))
+            .log()
+            .then()
+            .`as`(StepVerifier::create )
+            .expectComplete()
+            .verify()
+
+    }
+
+}
