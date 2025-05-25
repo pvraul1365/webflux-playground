3f0c88caa82bbe7b73696d77b7794a3ed9b0a16a
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-05-25 19:00:58 +0200

    119. Product Client

---
+++ src/test/kotlin/com/rperezv/webflux_playground/sec08/ProductClient.kt
@@ -0,0 +1,25 @@
+package com.rperezv.webflux_playground.sec08
+
+import com.rperezv.webflux_playground.sec08.dto.ProductDto
+import com.rperezv.webflux_playground.sec08.dto.UploadResponse
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+class ProductClient {
+
+    private val client: WebClient = WebClient.builder()
+        .baseUrl("http://localhost:8080")
+        .build()
+
+    fun uploadProducts(flux: Flux<ProductDto>): Mono<UploadResponse> {
+        return client.post()
+            .uri("/api/v3/products/upload")
+            .contentType(MediaType.APPLICATION_NDJSON)
+            .body(flux, ProductDto::class.java)
+            .retrieve()
+            .bodyToMono(UploadResponse::class.java)
+    }
+
+}
