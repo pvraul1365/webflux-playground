33fd4d6dab3fe09050c32a20152094a25e3367a2
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-05-31 10:19:43 +0200

    147. Configuring Connection Pool Size

--- src/test/kotlin/com/rperezv/webflux_playground/sec10/Lec01HttpConnectionPoolingTest.kt
+++ src/test/kotlin/com/rperezv/webflux_playground/sec10/Lec01HttpConnectionPoolingTest.kt
@@ -2,30 +2,46 @@ package com.rperezv.webflux_playground.sec10
 
 import com.rperezv.webflux_playground.sec10.AbstractWebClient
 import com.rperezv.webflux_playground.sec10.dto.Product
+import kotlinx.coroutines.newFixedThreadPoolContext
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.netty.resources.ConnectionProvider
 import reactor.test.StepVerifier
+import reactor.netty.http.client.HttpClient
 import java.time.Duration
 
 class Lec01HttpConnectionPoolingTest : AbstractWebClient() {
 
-    private val client: WebClient = super.createWebClient()
+    private val client: WebClient = super.createWebClient{ b ->
+        val poolSize = 501
+        val provider = ConnectionProvider.builder("vins")
+            .lifo()
+            .maxConnections(poolSize)
+            .pendingAcquireMaxCount(poolSize * 5)
+            .build()
+
+        val httpClient = HttpClient.create(provider)
+            .compress(true)
+            .keepAlive(true)
+
+        b.clientConnector(ReactorClientHttpConnector(httpClient))
+    }
 
     @Test
     fun concurrentRequest() {
-        val max = 3
+        val max = 501
         Flux.range(1, max)
-            .flatMap(this::getProduct)
+            .flatMap(this::getProduct, max)
             .collectList()
             .`as`(StepVerifier::create)
             .assertNext { products -> Assertions.assertEquals(max, products.size) }
             .expectComplete()
             .verify()
 
-        Thread.sleep(Duration.ofMinutes(1))
     }
 
     private fun getProduct(id: Int): Mono<Product> {
