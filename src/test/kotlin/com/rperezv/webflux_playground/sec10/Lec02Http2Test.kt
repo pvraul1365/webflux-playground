5767623a27738e3765187360e8c7f34b67e355d7
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-05-31 10:44:23 +0200

    151. HTTP2 - Demo

---
+++ src/test/kotlin/com/rperezv/webflux_playground/sec10/Lec02Http2Test.kt
@@ -0,0 +1,52 @@
+package com.rperezv.webflux_playground.sec10
+
+import com.rperezv.webflux_playground.sec10.dto.Product
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.netty.http.HttpProtocol
+import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
+import reactor.test.StepVerifier
+
+class Lec02Http2Test : AbstractWebClient() {
+
+    private val client: WebClient = super.createWebClient{ b ->
+        val poolSize = 1
+        val provider = ConnectionProvider.builder("vins")
+            .lifo()
+            .maxConnections(poolSize)
+            .build()
+
+        val httpClient = HttpClient.create(provider)
+            .protocol(HttpProtocol.H2C)
+            .compress(true)
+            .keepAlive(true)
+
+        b.clientConnector(ReactorClientHttpConnector(httpClient))
+    }
+
+    @Test
+    fun concurrentRequest() {
+        val max = 10000
+        Flux.range(1, max)
+            .flatMap(this::getProduct, max)
+            .collectList()
+            .`as`(StepVerifier::create)
+            .assertNext { products -> Assertions.assertEquals(max, products.size) }
+            .expectComplete()
+            .verify()
+
+    }
+
+    private fun getProduct(id: Int): Mono<Product> {
+        return client.get()
+            .uri("/product/{id}", id)
+            .retrieve()
+            .bodyToMono(Product::class.java)
+    }
+
+}
